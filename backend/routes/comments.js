1d54777142555a7ee033021a422d48e46da9e5e1
autonomousinfantry <43073131+autonomousinfantry@users.noreply.github.com>
2024-06-09 02:58:14 +0300

    Delete backend directory

--- backend/routes/comments.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const commentController = require('../controllers/commentController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/', protect, commentController.addComment);
-router.get('/:fileId', protect, commentController.getCommentsByFile);
-
-module.exports = router;
--- backend/routes/files.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const fileController = require('../controllers/fileController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/upload', protect, fileController.uploadFile);
-router.get('/:projectId', protect, fileController.getFilesByProject);
-
-module.exports = router;
