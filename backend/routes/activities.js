21c58af4234d3cfdf5de1b885e3aec1dd91ce026
autonomousinfantry <43073131+autonomousinfantry@users.noreply.github.com>
2024-06-09 20:20:30 +0300

    Delete backend directory

--- backend/routes/activities.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const activityController = require('../controllers/activityController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.get('/:projectId', protect, activityController.getActivitiesByProject);
-
-module.exports = router;
