1fe90d7383c0cb2a1ec15757d68ea86bc1d296e2
autonomousinfantry <43073131+autonomousinfantry@users.noreply.github.com>
2024-06-09 20:21:02 +0300

    Delete frontend directory

--- frontend/src/components/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = localStorage.getItem('token'); // veya başka bir oturum kontrolü
-  return isAuthenticated ? children : <Navigate to="/signin" />;
-};
-
-export default ProtectedRoute;
