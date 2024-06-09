53dc934ea2d313567546a7b43b0ae9b2696baa1a
autonomousinfantry <43073131+autonomousinfantry@users.noreply.github.com>
2024-06-09 19:36:20 +0300

    Delete backend directory

--- backend/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getProfile, updateProfile } = require('../controllers/userController');
-const { protect } = require('../middleware/auth');
-
-router.route('/profile').get(protect, getProfile).put(protect, updateProfile);
-
-module.exports = router;
