377419f6300ad16a4ebb35d5a8a5a669fd75dc01
autonomousinfantry <43073131+autonomousinfantry@users.noreply.github.com>
2024-06-10 10:45:52 +0300

    Delete backend directory

--- backend/routes/notifications.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const notificationController = require('../controllers/notificationController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.get('/user/:userId', protect, notificationController.getNotificationsByUser);
-router.patch('/:id/read', protect, notificationController.markAsRead);
-
-module.exports = router;
