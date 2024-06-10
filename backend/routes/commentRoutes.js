0f65160b6d7a3f0ba95372dc57f8fc6160539103
autonomousinfantry <43073131+autonomousinfantry@users.noreply.github.com>
2024-06-10 14:58:05 +0300

    Delete backend directory

--- backend/routes/commentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addComment, getCommentsByVideo } = require('../controllers/commentController');
-const { protect } = require('../middlewares/authMiddleware');
-
-router.post('/', protect, addComment);
-router.get('/:videoId', protect, getCommentsByVideo);
-
-module.exports = router;
